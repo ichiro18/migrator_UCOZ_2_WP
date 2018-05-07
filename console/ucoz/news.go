99d2ad49267ba220a86f26949c7bb1c66e0466a2
ichiro18 <ichirozcom@gmail.com>
2018-05-07 16:21:14 +0300

    clear html without comments

--- console/ucoz/news.go
+++ console/ucoz/news.go
@@ -7,6 +7,9 @@ import (
 
 	"strings"
 
+	"encoding/json"
+
+	"github.com/fatih/color"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 )
@@ -18,6 +21,9 @@ var NewsCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		site := new(Site)
 		site.parseNews(UcozFileStruct.Site.Path)
+		config := Env.Config.GetStringMapString("ucoz")
+		ucozPath := checkFolder(config["path"])
+		site.SaveNews(UcozFileStruct, filepath.Join(ucozPath, "work"))
 	},
 }
 
@@ -26,17 +32,13 @@ type Site struct {
 }
 
 type NewItem struct {
-	ID          string
-	CAT_ID      string
-	URL_YEAR    string
-	URL_MONTH   string
-	URL_DAY     string
-	TITLE       string
-	SNIPPET     string
-	MESSAGE     string
-	ATTACHMENTS string
-	VIEWS_COUNT string
-	URL_ALIAS   string
+	ID        string
+	CAT_ID    string
+	URL_YEAR  string
+	URL_MONTH string
+	URL_DAY   string
+	TITLE     string
+	MESSAGE   string
 }
 
 func (self *Site) parseNews(path string) {
@@ -74,13 +76,41 @@ func (self *Site) Load(path string) {
 			URL_MONTH: item[3],
 			URL_DAY:   item[4],
 			TITLE:     item[11],
-			MESSAGE:   item[13],
+			MESSAGE:   stripHtmlComment(item[13]),
 		}
 		self.News = append(self.News, newsItem)
 	}
 }
 
-func GetNews() {
-	site := new(Site)
-	site.parseNews(UcozFileStruct.Site.Path)
+func (self *Site) SaveNews(structure *ucozFileStruct, path string) {
+	if self.News == nil {
+		self.Load(structure.Site.Path)
+	}
+	data := self.News
+	dataJson, err := json.MarshalIndent(data, " ", "  ")
+	if err != nil {
+		fmt.Errorf("Can't convert data to json. ")
+	}
+	afero.WriteFile(Env.FileSystem, filepath.Join(path, "news.json"), dataJson, 0755)
+	color.Green("File '%v' created", filepath.Join(path, "news.json"))
+}
+
+func stripHtmlComment(html string) string {
+	count := strings.Count(html, "<!--")
+	if count != 0 {
+		for i := 1; i <= count; i++ {
+			startIndex := strings.Index(html, "<!--")
+			endIndex := strings.Index(html, "-->") + 3
+			html = strings.Replace(html, html[startIndex:endIndex], "", 1)
+		}
+	}
+	return html
+}
+
+func GetNews(self *Site) []NewItem {
+	if self.News == nil {
+		self.Load(UcozFileStruct.Site.Path)
+	}
+
+	return self.News
 }
